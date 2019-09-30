01e0b61723390524c8ae043285ebb84f1c620ec8
Chris Hurt <chrishcoding@gmail.com>
2019-09-30 19:05:55 +1000

    Updates

--- precache-manifest.d0fcd37336fda4dc4498100e47314f30.js
+++ precache-manifest.d0fcd37336fda4dc4498100e47314f30.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "509d7779ee2624f4f0f4c3a96755cb01",
+    "revision": "9767568db335086cc7aab2d1533ea6c9",
     "url": "/React-Recall/index.html"
   },
   {
-    "revision": "d9f77015e7d211424178",
-    "url": "/React-Recall/static/css/main.f2d52729.chunk.css"
+    "revision": "1e82433c6970f51f1c94",
+    "url": "/React-Recall/static/css/main.647ee571.chunk.css"
   },
   {
     "revision": "4458eee00316460d5456",
     "url": "/React-Recall/static/js/2.4380cc17.chunk.js"
   },
   {
-    "revision": "d9f77015e7d211424178",
-    "url": "/React-Recall/static/js/main.e23e614e.chunk.js"
+    "revision": "1e82433c6970f51f1c94",
+    "url": "/React-Recall/static/js/main.627b0d08.chunk.js"
   },
   {
     "revision": "c99f9417aa04de85f5fb",
