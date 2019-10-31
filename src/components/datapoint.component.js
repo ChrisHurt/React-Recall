d1e975aedc5afe6bdad49a0943f671a563cdfa1e
Chris Hurt <chrishcoding@gmail.com>
2019-10-31 15:22:54 +1100

    Separated datapoint component styling into scss file

--- src/components/datapoint.component.js
+++ src/components/datapoint.component.js
@@ -40,11 +40,8 @@ export default class DataPoint extends React.Component {
       <div className="datapoint-rotation-container"
         style={
           {
-            position: "relative",
             width: `${radialDisplacement}px`,
             transform: `translate(${parentDiameter + transformOffset}px,${parentDiameter + transformOffset}px) rotate(${rotationAngle}deg)`,
-            transformOrigin: 'left center',
-            height: 0,
             zIndex: (radialDisplacement===0) ? (4)  : zIndex+1,
             top,
             left
@@ -57,19 +54,10 @@ export default class DataPoint extends React.Component {
             {
               backgroundImage: `url(${image_url})`,
               backgroundSize,
-              position: 'relative',
               width: `${diameter}px`,
               height: `${diameter}px`,
-              // lineHeight: `${diameter/2}px`,
               transform: `translate(${radialDisplacement - (diameter / 2)}px,${-diameter / 2}px) rotate(-${rotationAngle}deg)`,
-              zIndex,
-              transition: 'transform 0.35s ease-in, width 0.35s ease-in, height 0.35s ease-in',
-              color: '#2C3531',
-              fontWeight: '700',
-              textShadow: '1px 1px #D1E8E2',
-              textAlign: 'center',
-              fontSize: '1.5em',
-              textWrap: 'wrap'
+              zIndex
             }
           }
           onClick={
@@ -85,52 +73,41 @@ export default class DataPoint extends React.Component {
                 height: `${diameter}px`,
               }}
             >
-            <div style={{
-              userSelect: 'none',              
-            }}><span
-              style={{
-                fontSize: ``,
-                backgroundColor: 'rgba(255,203,154,0.65)',
-                height: `${diameter / 3}px`,
-                lineHeight: `${diameter / 3}px`
-              }}
+            <div className = "not-selectable">
+              <span
+                className="background-color-light-orange"
+                style={{
+                  height: `${diameter / 3}px`,
+                  lineHeight: `${diameter / 3}px`
+                }}
             >Remember<br></br>{text.split('').slice(0,16).join('')+ '?'}</span></div>
             <button
+              className="not-selectable datapoint-button-remembered"
               style={{
                 width: 7*diameter/18,
-                userSelect: 'none',
-                zIndex: zIndex+1,
-                padding: '0.25em',
-                backgroundColor: '#FFCB9A',
-                borderRadius: '0.5em',
-                marginRight: '0.5em'
+                zIndex: zIndex+1
               }}
               onClick={
                 ()=>{
-                  removeDataByKey(this.props.id,1)
-                  allowTransitions()
-                  incrementSuccess()
-                  // this.props.recordGuess(1,this.props.id,text)
+                  removeDataByKey(this.props.id,1);
+                  allowTransitions();
+                  incrementSuccess();
                 }
               }
             >
               I remember
             </button>
-            <button 
+            <button
+              className="not-selectable datapoint-button-forgot" 
               style={{
                 width: 7*diameter/18,
-                userSelect: 'none',
                 zIndex: zIndex+1,
-                padding: '0.25em',
-                backgroundColor: '#D9B08C',
-                borderRadius: '0.5em'
               }}
               onClick={
                 ()=>{
                   removeDataByKey(this.props.id,0)
                   allowTransitions()
                   incrementFailure()
-                  // this.props.recordGuess(0,this.props.id,text)
                 }
               }
             >
